fcfc9dfe2581ec1d7f1074a74b6e5d76c57dcef4
ekolpakov <ekolpakov@greet-go.com>
2021-01-06 13:17:26 +0600

    Удалена часть старой реализации

--- greetgo.conf/src/kz/greetgo/conf/hot/HotConfigConstants.java
+++
@@ -1,7 +0,0 @@
-package kz.greetgo.conf.hot;
-
-public class HotConfigConstants {
-
-  public static final String COUNT_SUFFIX = "listElementsCount";
-
-}
--- greetgo.conf/src/kz/greetgo/conf/hot/ReadElement.java
+++
@@ -1,9 +0,0 @@
-package kz.greetgo.conf.hot;
-
-public interface ReadElement {
-
-  String fieldName();
-
-  Object fieldValue();
-
-}
