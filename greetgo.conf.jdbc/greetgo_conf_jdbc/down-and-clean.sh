4c775d890db09381e93ba18c5950a700b81bfeaf
ekolpakov <ekolpakov@greet-go.com>
2021-01-10 17:42:44 +0600

    Добавлены скрипты инициации тестовых БД

--- greetgo.conf.jdbc/greetgo_conf_jdbc/down-and-clean.sh
+++ greetgo.conf.jdbc/greetgo_conf_jdbc/down-and-clean.sh
@@ -2,17 +2,11 @@
 
 cd "$(dirname "$0")" || exit 131
 
-
-
 docker-compose down
-
-mkdir -p volumes
 
 docker run --rm -i \
   -v "$PWD/volumes:/volumes" \
   busybox:1.31.0 \
   find /volumes/ -maxdepth 1 -mindepth 1 -exec rm -rf {} \;
 
-
-
-docker-compose up -d
+rm -rvf volumes
