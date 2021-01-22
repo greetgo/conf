4e446d1967c8cffaa8296ebb20dfefad4a0144e0
ekolpakov <ekolpakov@greet-go.com>
2021-01-22 08:54:06 +0600

    Перенёс docker-compose выше по дереву директорий проекта

---
+++ greetgo.conf.zookeeper/src/kz/greetgo/conf/zookeeper/Zookeeper.java
@@ -0,0 +1,7 @@
+package kz.greetgo.conf.zookeeper;
+
+public class Zookeeper {
+  public void zoo() {
+    System.out.println("Hi");
+  }
+}
---
+++ greetgo.conf.zookeeper/test_src/kz/greetgo/conf/zookeeper/ZookeeperTest.java
@@ -0,0 +1,20 @@
+package kz.greetgo.conf.zookeeper;
+
+import org.testng.annotations.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ZookeeperTest {
+
+  @Test
+  public void name() {
+
+    //
+    //
+    new Zookeeper().zoo();
+    //
+    //
+
+    assertThat(1);
+  }
+}
